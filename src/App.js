14c347d8a21a65b53bff60671ef446368a66062a
qqis314860100 <18876309817@163.com>
2019-12-29 01:28:36 +0800

    1、路由的配置和应用部分
    2、公共组件的开发
    
    3、redux 的 store 创建和引入

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { renderRoutes } from 'react-router-config';
+import { HashRouter } from 'react-router-dom';
+import { IconStyle } from './assets/iconfont/iconfont';
 import { GlobalStyle } from './style';
+import routes from './routes';
+import store from './store';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle />
+        <IconStyle />
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
+  );
 }
 
 export default App;
